bac32bae9a9c1c57ab4939820feb11df19a56161
Siddhant B. Ingale <165244646+siddhantingale@users.noreply.github.com>
2025-04-04 16:50:36 +0000

    Datatypes_and_Veriable

---
+++ Basics/Veriables.js
@@ -0,0 +1,18 @@
+const accountId=12345
+let accountEmail="siihn@gmail"
+var accountPass="123@Siddhant"
+
+//accountId=2 not allowed
+accountCity="jaypur"
+console.log("accountId");
+accountEmail="Akash@gmail.com"
+console.log(accountEmail);
+let city;
+
+/*If you are only declear a veriable then its value become a UNDEFINED*/
+
+/* Prefer use of var because issue of scope*/
+console.table([accountId,accountEmail,accountPass,accountCity,city]);
+console.log(city);
+
+
---
+++ Basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//alert("Hi Siddhant")
+// We are using nodejs it will run on browser
+
+let name="Siddhant"
+let age=20
+let weaight=45
+console.log(name);
+
+//String=" ";
+//null-- standaloan value
+//undefined --
+//symbol--Uniqueness
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+//typeof null is object 
+//typeof undefined is undefined
+
