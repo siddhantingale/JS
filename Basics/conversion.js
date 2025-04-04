e381e9cfaf253d841e5fa6d4168138806691cf02
Siddhant B. Ingale <165244646+siddhantingale@users.noreply.github.com>
2025-04-04 18:15:12 +0000

    Conversion

---
+++ Basics/conversion.js
@@ -0,0 +1,59 @@
+let core="33abc"
+// console.log(typeof score);
+
+ let valueInNum=Number(core)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber); //this gives output as NaN-->i.e. Not a Number
+
+
+let score=null
+//console.log(typeof score);
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);//null gives output as 0
+
+let scor=undefined
+//console.log(typeof scor);
+
+let valueINumber=Number(scor)
+//console.log(typeof valueINumber);
+
+//console.log(valueINumber);//undefined gives the output as NaN
+
+let sco=true
+//console.log(typeof sco);
+
+let valueNumber=Number(sco)
+//console.log(typeof valueNumber);
+
+//console.log(valueNumber);
+
+//"33"-->33
+//"33abc"-->NaN
+//true-->1 False-->0
+//Null-->0
+//undefined-->NaN
+
+let isLo=1
+let boolILog=Boolean(isLo)
+//console.log(boolIsLog);
+
+let isLog=""
+let boolIsLog=Boolean(isLog)
+//console.log(boolIsLog);
+
+//1-->true
+//""-->false
+//"String"-->true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
